d4c4cdefc73cf50bdafc52f8fc85af3137c775b0
Adhon <adhonrom@gmail.com>
2018-02-18 19:42:27 +0700

    revisi

--- Exercise15-w3-mengelompokkanhewan.js
+++ Exercise15-w3-mengelompokkanhewan.js
@@ -1,55 +1,30 @@
 
-// function groupAnimals(animals) {
-//   animals.sort()
-  
-//   var i = 0;
-//   var j = animals.length - 1
-  
-//   var output = [];
-  
-//   while (animals.length>0) {
-//     var temporary = []
-//     var firstLetter = animals[0][0];
-//     temporary.push(animals[0])
-//     animals.splice(0, 1)
-    
-//     while (animals.length > 0 && firstLetter === animals[0][0]) {
-//       temporary.push(animals[0]);
-//       animals.splice(0, 1);
-//     }
-    
-//     output.push(temporary);
-//   }
-//   return output
-// }
+function groupAnimals(animals) {
+  animals.sort()
 
-// // TEST CASES
-// console.log(groupAnimals(['cacing', 'ayam', 'anjing', 'kuda', 'anoa', 'kancil']));
-// // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
-// console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
-// // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
+  var i = 0;
+  var j = animals.length-1
 
+  var output = [];
 
+  while (animals.length>0) {
+    var tampung = []
+    var hurufAwal = animals[0][0];
+    tampung.push(animals[0])
+    animals.splice(0, 1)
 
+    while (animals.length > 0 && hurufAwal === animals[0][0]) {
+      tampung.push(animals[0]);
+      animals.splice(0, 1);
+    }
 
+    output.push(tampung);
+  }
+  return output
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// TEST CASES
+console.log(groupAnimals(['cacing', 'ayam', 'anjing', 'kuda', 'anoa', 'kancil']));
+// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
